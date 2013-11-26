ee757a0162cf5a417051c6cefb67715549b130bb
jasmine <jasminezhou27@gmail.com>
2013-11-26 12:48:43 -0800

    improveing contact.php
    improveing contact.php

--- bootstrapwp-child/jasmine-contact.php
+++ bootstrapwp-child/jasmine-contact.php
@@ -27,31 +27,21 @@ get_header(); ?>
             <h1><?php the_title();?></h1>
         </header>
 
-        <?php the_post_thumbnail('full');?>
+        
         <div class="row content">
-
-            <?php the_content();   ?>
-
-<!--<div class="span5">
-<center class="center">
-<form action="MAILTO:jasminezhou27@gmail.com" method="post" enctype="text/plain">
-Name:<br>
-<input type="text" name="name" value="your name"><br>
-E-mail:<br>
-<input type="text" name="mail" value="your email"><br>
-Comment:<br>
-<input type="text" name="comment" value="your comment" size="50"><br><br>
-<input type="submit" value="Send">
-<input type="reset" value="Reset">
-</form>
-
-<img class="envelope-front" src="http://localhost:8888/dev/portfolio/wp-content/uploads/2013/11/envelope-front.png" alt="envelope-front" width="304" height="166" class="alignnone size-full wp-image-325" />
-</center>
+<div class="span4">
+<p><img class="icons phone" src="<?php echo get_stylesheet_directory_uri(); ?>/imgs/phone.png" alt="phone">778 387 1111</p>
+<p><img class="icons email" src="<?php echo get_stylesheet_directory_uri(); ?>/imgs/email.png" alt="email">jingdesignstudio@gmail.com</p>
+<img class="icons" src="<?php echo get_stylesheet_directory_uri(); ?>/imgs/p.png" alt="linkin">
+<img class="icons" src="<?php echo get_stylesheet_directory_uri(); ?>/imgs/in.png" alt="linkin">
+<img class="icons" src="<?php echo get_stylesheet_directory_uri(); ?>/imgs/twetter.png" alt="linkin">
 </div>
--->
 
-
-
+<div class="span5"><center class="center">
+<form class="animated fadeInUp">
+            <?php the_content(); ?>
+</form>
+<div class="span5">
             <?php endwhile; // end of the loop. ?>
 
         </div>
